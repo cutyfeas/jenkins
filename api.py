ebd4ac243b8ffa6d2b859e34a6bc0438496e75b5
vishal kumar <vishal.kumar061@gmail.com>
2022-06-23 09:54:57 +0530

    api

--- api.py
+++ api.py
@@ -1,1 +1,5 @@
+import shutil
+from_directory = r"C:\ProgramData\Jenkins\.jenkins\workspace\jenkinspipeline\WebApplication1\bin"
+to_directory = r"C:\ProgramData\Jenkins\.jenkins\workspace\jenkinspipeline\PROD\bin"
 
+shutil.copytree(from_directory, to_directory)
